6848420b4b4e3dacf11f1e49dbc267916c8e0d59
Uchiha@8 <2110107@newuu.uz>
2025-02-19 19:21:38 +0500

    Add file parsing logic to extract Profile data
    Implemented a method to parse a file and populate a Profile object with data such as Name, Age, Email, and Phone. The method uses BufferedReader to read the file line by line and stores the extracted key-value pairs in a map for processing. Error handling via try-catch ensures exceptions are properly caught during file reading.

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,32 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String pathFile = file.getPath();
+        Map<String, String> profileData = new HashMap<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(pathFile))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] data = line.split(": ");
+                profileData.put(data[0].trim(), data[1].trim());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Profile profile = new Profile(
+                profileData.get("Name"),
+                Integer.parseInt(profileData.get("Age")),
+                profileData.get("Email"),
+                Long.parseLong(profileData.get("Phone"))
+        );
+        return profile;
     }
 }
